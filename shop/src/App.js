912a6959c52fad4e4188322b31d9acd98d314e9a
jk_ntb <hynb12@naver.com>
2023-12-31 18:22:21 +0900

    20231231

--- shop/src/App.js
+++ shop/src/App.js
@@ -7,42 +7,108 @@ import bg from "./img/bg.png";
 import Row from "react-bootstrap/Row";
 import { useState } from "react";
 import data from "./data.js";
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./routes/Detail.js";
 
 // 페이지나느눈법(리엑트)
 // 1. 컴포넌트 만들어서 상세페이지 내용 채움
 // 2. 누가 /detail 접속하면 그 컴포넌트 보여줌
-
 function App() {
-  <Routes>
-    <route path="/detail" element={<div>상세페이지임</div>}></route>
-    <route />
-  </Routes>;
-
   let [shoes, setShoes] = useState(data);
+  //  페이지 이동기능을 만들고 싶으면 useNavigate() 씁니다.
+  let navigate = useNavigate();
 
   return (
     <div className="App">
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand>ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                // navigate(-1); // 뒤로가기(-2하면 2번감)
+                navigate("/");
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              Detail
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/event");
+              }}
+            >
+              Event
+            </Nav.Link>
+            {/* <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/detail">detail</Nav.Link> */}
           </Nav>
         </Container>
       </Navbar>
-
       <div className="main-bg" style={{ backgroundImage: `url(${bg})` }}></div>
-      <Container>
-        <Row>
-          {shoes.map((a, i) => {
-            return <Card a={a} i={i} />;
-          })}
-        </Row>
-      </Container>
+      {/* <Link to="/">홈</Link>
+      <Link to="/detail">상세페이지</Link> */}
+
+      <Routes>
+        <Route path="" element={<MainPage shoes={shoes} />} />
+        <Route path="detail" element={<Detail />} />
+        <Route path="about" element={<About />}>
+          {/* 
+          <Route>안에 <Route>를 넣을 수 있는데 이걸 Nested routes 라고 부릅니다.
+          /about/member 
+          /about/location
+          */}
+          <Route path="member" element={<div>멤버임</div>} />
+          <Route path="location" element={<About />} />
+        </Route>
+        <Route path="event" element={<EventPage />}>
+          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>} />
+          <Route path="two" element={<p>생일기념 쿠폰받기</p>} />
+        </Route>
+        {/* 404페이지는 */}
+        <Route path="*" element={<div>없는페이지에요</div>} />
+      </Routes>
     </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      {/* 
+      /about/member로 접속시 <Outlet>자리에 아까의 <div> 박스들이 잘 보입니다. 
+      그래서 유사한 서브페이지들이 많이 필요하다면 이렇게 만들어도 됩니다.
+      */}
+      <Outlet></Outlet>
+    </div>
+  );
+}
+
+function EventPage() {
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+
+function MainPage({ shoes }) {
+  return (
+    <Container>
+      <Row>
+        {shoes.map((a, i) => {
+          return <Card product={a} idx={i} />;
+        })}
+      </Row>
+    </Container>
   );
 }
 
@@ -50,12 +116,12 @@ function Card(props) {
   return (
     <div className="col-md-4">
       <img
-        src={process.env.PUBLIC_URL + "/img/shoes" + (props.i + 1) + ".jpg"}
+        src={process.env.PUBLIC_URL + "/img/shoes" + (props.idx + 1) + ".jpg"}
         width="80%"
       />
-      <h4>{props.a.title}</h4>
-      <p>{props.a.content}</p>
-      <p>{props.a.price}</p>
+      <h4>{props.product.title}</h4>
+      <p>{props.product.content}</p>
+      <p>{props.product.price}</p>
     </div>
   );
 }
